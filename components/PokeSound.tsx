4df00fab6e7381b53d83758a5ecdc8478bc7cb84
jjmmhola2 <jjmmhola2@gmail.com>
2025-10-29 19:22:57 +0000

    En la carpeta de components, agregué un archivo llamado PokeSound, para poder reproducir el sonido de cada pokemon -Se instalo el expo-av, para poder reproducir el audio con "npx expo install expo-av"

---
+++ components/PokeSound.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PokeSound = () => {
+  return (
+    <View>
+      <Text>PokeSound</Text>
+    </View>
+  )
+}
+
+export default PokeSound
